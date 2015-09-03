3b9ed2e4b9ee6aaa794d24744fbac0f8f403e0a3
Torben Köhn <t.koehn@outlook.com>
2015-09-03 17:21:50 +0200

    Changed: New TF App config style

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include 'vendor/autoload.php';
 //The first parameter to App can actually be omitted completely,
 //if the app-directory is ./app from here!
 $app = new App([
-    'path' =>__DIR__.'/app'
+    'path' =>__DIR__.'/app',
 ]);
 
-var_dump($app);
+$app->run();
